03f81f466f788da795b595c19ef7710b33c7ceb1
Cagri Turk <haliscagri@gmail.com>
2018-12-06 01:28:07 +0300

    JSON data added

--- github.py
+++ github.py
@@ -1,12 +1,20 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import requests
 
 app = Flask(__name__)
-
-@app.route("/")
+base_url = "https://api.github.com/users/"
+@app.route("/", methods = ["GET","POST"] )
 
 def index():
-	return render_template("index.html")
-	#return "Hello, World"
+	if request.method == "POST":
+		githubname = request.form.get("githubname")
+		response = request.get(base_url + githubname)
+		user_info = response.json()
+		return render_template("index.html" , profile = user_info)
+
+	else:
+		return render_template("index.html")
+		#return "Hello, World"
 
 if __name__ == "__main__":
 	app.run(debug = True, host='0.0.0.0')
